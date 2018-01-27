529d87293ea366f44b741f346c200258760a1100
G-noname <xigangok@163.com>
2018-01-27 16:50:24 +0800

    20180127save

--- jobplus3-7/jobplus/app.py
+++ jobplus3-7/jobplus/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 from flask_login import LoginManager
+from flask_migrate import Migrate
 from jobplus.models import db, User
 from jobplus.config import configs
 
 def register_extensions(app):
     db.init_app(app)
+    Migrate(app, db)
     login_manager = LoginManager()
     login_manager.init_app(app)
 
